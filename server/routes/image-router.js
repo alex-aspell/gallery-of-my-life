49b37639f906c455adfb0dc5663b5ac5327d5b88
Alex Aspell <alexgaspell@gmail.com>
2018-02-25 14:55:17 -0600

    bootstrap coming along

--- server/routes/image-router.js
+++ server/routes/image-router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/', (request, response) => {
-    sqlText = 'SELECT * FROM images';
+    sqlText = 'SELECT * FROM images ORDER BY id ASC';
     pool.query(sqlText)
     .then((result) => {
         console.log('got the stuff', result);
